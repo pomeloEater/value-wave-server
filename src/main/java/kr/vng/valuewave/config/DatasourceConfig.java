14c29b2009cd12e48dcac77fa3b106dfa9c3f591
jinhui.dev <jinhui.dev@gmail.com>
2022-05-27 15:19:39 +0900

    Config: MyBatis 설정 추가 및 디렉토리 변경

--- src/main/java/kr/vng/valuewave/config/DatasourceConfig.java
+++ src/main/java/kr/vng/valuewave/config/DatasourceConfig.java
@@ -6,6 +6,7 @@ import org.apache.ibatis.session.SqlSessionFactory;
 import org.mybatis.spring.SqlSessionFactoryBean;
 import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.*;
 import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
@@ -33,11 +34,13 @@ public class DatasourceConfig {
     }
 
     @Bean
-    public SqlSessionFactory sqlSessionFactory(DataSource customDataSource) throws Exception {
+    public SqlSessionFactory sqlSessionFactory(DataSource customDataSource, ApplicationContext applicationContext) throws Exception {
         final SqlSessionFactoryBean sqlSessionFactory = new SqlSessionFactoryBean();
         sqlSessionFactory.setDataSource(customDataSource);
-        PathMatchingResourcePatternResolver resolver = new PathMatchingResourcePatternResolver();
-        sqlSessionFactory.setMapperLocations(resolver.getResources("classpath:mapper/**/*.xml"));
+        // configuration 적용 및 mapper 위치 설정 등
+        sqlSessionFactory.setConfigLocation(applicationContext.getResource("classpath:mybatis/mybatis-config.xml"));
+        sqlSessionFactory.setMapperLocations(new PathMatchingResourcePatternResolver().getResources("classpath:mybatis/mapper/**/*.xml"));
+        sqlSessionFactory.setTypeAliasesPackage("kr.vng.valuewave.web"); /*잘 작동하는지 확인*/
         return sqlSessionFactory.getObject();
     }
 
