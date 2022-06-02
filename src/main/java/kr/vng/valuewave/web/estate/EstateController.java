afd421ec791203bd6dfc47700a73195d910444e2
jinhui.dev <jinhui.dev@gmail.com>
2022-06-02 17:03:49 +0900

    Feat: 부동산종합정보 Controller 메서드 추가 / Fix: sql 주석 수정, logging 경로 수정

--- src/main/java/kr/vng/valuewave/web/estate/EstateController.java
+++ src/main/java/kr/vng/valuewave/web/estate/EstateController.java
@@ -17,9 +17,64 @@ public class EstateController {
     private final EstateService estateService;
     private static final Logger LOGGER = LogManager.getLogger(EstateController.class);
 
+    /**
+     * 토지(임야) 대장 조회
+     * @param pnu
+     * @return
+     */
     @GetMapping("/get-land-book/{pnu}")
     public Object getLandBook(@PathVariable String pnu) {
-        return null;
+        return ResultMapUtil.success(estateService.getLandBook(pnu));
+    }
+
+    /**
+     * 건축물 대장 조회
+     * @param pnu
+     * @return
+     */
+    @GetMapping("/get-building-book/{pnu}")
+    public Object getBuildingBook(@PathVariable String pnu) {
+        return ResultMapUtil.success(estateService.getBuldBookByPnu(pnu));
+    }
+
+    /**
+     * 개별주택가격 조회
+     * @param pnu
+     * @return
+     */
+    @GetMapping("/get-individual-price/{pnu}")
+    public Object getIndividualPrice(@PathVariable String pnu) {
+        return ResultMapUtil.success(estateService.getIndividualHousePrice(pnu));
+    }
+
+    /**
+     * 공동주택가격 조회
+     * @param pnu
+     * @return
+     */
+    @GetMapping("/get-apartment-price/{pnu}")
+    public Object getApartmentPrice(@PathVariable String pnu) {
+        return ResultMapUtil.success(estateService.getApartmentHousePrice(pnu));
+    }
+
+    /**
+     * 공시지가 조회
+     * @param pnu
+     * @return
+     */
+    @GetMapping("/get-official-price/{pnu}")
+    public Object getOfficialPrice(@PathVariable String pnu) {
+        return ResultMapUtil.success(estateService.getOfficialLandPrice(pnu));
+    }
+
+    /**
+     * 토지이용현황 조회(임시))
+     * @param pnu
+     * @return
+     */
+    @GetMapping("/get-land-use/{pnu}")
+    public Object getLandUse(@PathVariable String pnu) {
+        return ResultMapUtil.failed();
     }
 
 }
